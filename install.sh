fc8ae82f26c1a4f65e0e65bbb372306a608e13a1
[sCRiPTz-TEAM] <scriptzteam@users.noreply.github.com>
2021-03-07 08:59:37 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+#Version: v1.0.0
+
+#Check
+if [ $# -eq 0 ]
+  then
+    echo "Run as - install.sh hidden_admin_dir"
+fi
+
+#Install
+apt update
+apt upgrade
+apt install unbound
+wget https://www.internic.net/domain/named.root -qO- | tee /var/lib/unbound/root.hints
+mv /etc/unbound/unbound.conf.d/pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf-backup
+wget https://raw.githubusercontent.com/scriptzteam/Public-Pi-hole/main/files/pi-hole.conf -O /etc/unbound/unbound.conf.d/pi-hole.conf
+systemctl disable unbound-resolvconf.service
+systemctl stop unbound-resolvconf.service
+systemctl restart dhcpcd
+service unbound restart
+mv /etc/resolv.conf /etc/resolv.conf-backup
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+chattr -f +i /etc/resolv.conf
+
+# Test validation
+# You can test DNSSEC validation using
+# dig sigfail.verteiltesysteme.net @127.0.0.1 -p 5335
+# dig sigok.verteiltesysteme.net @127.0.0.1 -p 5335
+# The first command should give a status report of SERVFAIL and no IP address.
+# The second should give NOERROR plus an IP address.
+
+echo "
+|***********************************************************************************|
+| Now you are going to install Pi-hole, as DNS use custom DNS (unbound) - 127.0.0.1 |
+|***********************************************************************************|
+"
+sleep 2
+# Pi-hole install
+curl -sSL https://install.pi-hole.net | bash
+
+# Move admin dir to dir specified in argument - hidden_admin_dir
+# Remove pihole dir
+mv /var/www/html/admin/ /var/www/html/$1
+rm -rf /var/www/html/pihole/
+
+# Unound stats:
+# unbound-control stats_noreset
